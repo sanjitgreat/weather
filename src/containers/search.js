a654c76f9153b52684b46d2710f793a79138ccbb
sanjitr <sanjit.rane@hotstar.com>
2018-03-21 21:43:09 +0530

    Initial commit

---
+++ src/containers/search.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index' 
+
+class search extends React.Component{
+ 	constructor(props){
+ 		super(props)
+ 		this.state={term:''}
+ 		this.onInputChange = this.onInputChange.bind(this)
+ 		this.onFormSubmit = this.onFormSubmit.bind(this)
+ 	}
+
+ 	onInputChange(e){
+ 		this.setState({term: e.target.value})
+ 	}
+
+ 	onFormSubmit(e){
+ 		e.preventDefault()
+ 		this.props.fetchWeather(this.state.term);
+ 		this.setState({term: ''})
+ 	}
+
+ 	render(){
+ 		return(
+ 				<form className='input-group' onSubmit={this.onFormSubmit}>
+ 					<input
+ 						value={this.state.term}
+ 						placeholder='Get a five-day forcast'
+ 						className='form-control'
+ 						onChange = {this.onInputChange}
+ 					 />
+ 					<span className='input-group-btn'>
+ 						<button type='submit' className='btn btn-secondary'>Submit</button>
+ 					</span>
+ 				</form>
+ 			)
+ 	}	
+ }
+
+ function mapDispatchToProps(dispatch){
+ 	return bindActionCreators({ fetchWeather }, dispatch);
+ }
+ export default connect(null, mapDispatchToProps)(search) 
